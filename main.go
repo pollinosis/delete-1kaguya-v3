fff67e01b20fe80c25e3042dbccb5bdcc15acafa
pollinosis <tau@fe2o3.jp>
2020-10-09 23:00:02 +0900

    🐢🍤🍦

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"net/url"
+	"log"
+	"os"
+	"net/http"
+
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	http.HandleFunc("/", d)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func deleteTimeLine(api *anaconda.TwitterApi, v url.Values) {
+	tweets, err := api.GetUserTimeline(v)
+	if err != nil {
+		panic(err)
+	}
+	for _, tweet := range tweets {
+		api.DeleteTweet(tweet.Id, true)
+	}
+}
+
+func d(w http.ResponseWriter, r *http.Request) {
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+	}
+    consumer_key := os.Getenv("CONSUMER_KEY")
+	consumer_secret := os.Getenv("CONSUMER_SECRET")
+	accsess_token := os.Getenv("ACCESS_TOKEN")
+	accsess_token_secret := os.Getenv("ACCESS_TOKEN_SECRET")
+	anaconda.SetConsumerKey(consumer_key)
+	anaconda.SetConsumerSecret(consumer_secret)
+	api := anaconda.NewTwitterApi(accsess_token, accsess_token_secret)
+
+    v := url.Values{}
+    v.Set("count", "200")
+
+	tweets, err := api.GetUserTimeline(v)
+	if err != nil {
+		panic(err)
+	}
+    for _, tweet := range tweets {
+		api.DeleteTweet(tweet.Id, true)
+	}
+}
